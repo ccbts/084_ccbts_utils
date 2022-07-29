2fa3f72aa934f9684ef89e26ac89df806f73a91c
alexandrosnic <alexnic31@gmail.com>
2022-07-29 14:52:14 +0200

    Installed ros2 api, vgc drivers and smach

--- webots_ros2/install_dependencies.sh
+++ webots_ros2/install_dependencies.sh
@@ -63,6 +63,15 @@ echo "Cocobots Webots environment installed successfully"
 git clone git@github.com:ccbts/092_ccbts_bringup.git src/ccbts_bringup
 echo "Cocobots UR launch files installed successfully"
 
+git clone git@github.com:alexandrosnic/vgc10_ros2_drivers.git src/vgc10_driver
+echo "VGC10 drivers installed successfully"
+
+git clone https://github.com/DeepX-inc/executive_smach.git src/executive_smach
+echo "ROS2 state machines (smach) installed successfully"
+
+git clone git@github.com:ccbts/093_ccbts_api.git src/ccbts_ros2_api
+echo "Cocobots ROS2 API installed successfully"
+
 colcon build
 source install/setup.bash
 export PYTHONPATH=${PYTHONPATH}:/home/${USER}/cocobots_ws/install/ccbts_environment/lib/python3.8/site-packages
