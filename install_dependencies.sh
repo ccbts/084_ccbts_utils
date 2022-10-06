58b2b2b8e586236034d1903bd8c53b8f25356114
alexandrosnic <alexnic31@gmail.com>
2022-10-06 15:12:14 +0200

    Updated the docker to install ROS2 and Webots at the container startup

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,6 +12,7 @@ sudo apt-get update && apt-get install -y \
         ros-foxy-ros2-controllers \
         ros-foxy-moveit \
         ros-foxy-warehouse-ros-mongo \
+        ros-foxy-webots-ros2-control \
         ros-foxy-tf-transformations \
         ros-foxy-srdfdom 
 
@@ -43,14 +44,16 @@ cd $DAI_WS
 # WORKDIR $DAI_WS/depthai
 # COPY requirements.txt .
 # RUN pip install -r requirements.txt
-echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | tee /etc/udev/rules.d/80-movidius.rules
-wget -qO- https://raw.githubusercontent.com/luxonis/depthai-ros/main/install_dependencies.sh | sudo bash
-wget https://raw.githubusercontent.com/luxonis/depthai-ros/main/underlay.repos
-vcs import src < underlay.repos
-rosdep install --from-paths src --ignore-src -r -y
-source /opt/ros/foxy/setup.bash
-colcon build 
-source install/setup.bash
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo apt install -y ros-foxy-depthai-ros
+# wget -qO- https://raw.githubusercontent.com/luxonis/depthai-ros/main/install_dependencies.sh | sudo bash
+# wget https://raw.githubusercontent.com/luxonis/depthai-ros/main/underlay.repos
+# vcs import src < underlay.repos
+# rosdep install --from-paths src --ignore-src -r -y
+# source /opt/ros/foxy/setup.bash
+# colcon build 
+# source install/setup.bash
 echo "depthAI driver installed successfully"
 
 # --------------------------- #
@@ -69,19 +72,28 @@ echo "Cocobots UR launch files installed successfully"
 git clone git@github.com:alexandrosnic/vgc10_ros2_drivers.git src/vgc10_driver
 echo "VGC10 drivers installed successfully"
 
+git clone git@github.com:ccbts/095_ccbts_motions.git src/ccbts_motions
+echo "Motions for the robot arm installed successfully"
+
 git clone https://github.com/DeepX-inc/executive_smach.git src/executive_smach
 echo "ROS2 state machines (smach) installed successfully"
 
 git clone git@github.com:ccbts/093_ccbts_api.git src/ccbts_ros2_api
+sudo mv src/ccbts_ros2_api/ccbts_api/include/ccbts_api/move_group_interface_improved.h /opt/ros/foxy/include/moveit/move_group_interface/
 echo "Cocobots ROS2 API installed successfully"
+
+git clone https://github.com/alexandrosnic/pymoveit2.git src/pymoveit2
+echo "Pymoveit2 (Python moveit wrapper) installed successfully"
+
+rosdep install -y -r -i --rosdistro ${ROS_DISTRO} --from-paths .
 
 colcon build
 source install/setup.bash
-export PYTHONPATH=${PYTHONPATH}:/home/${USER}/cocobots_ws/install/ccbts_environment/lib/python3.8/site-packages
+export PYTHONPATH=${PYTHONPATH}:/ccbts/cocobots_ws/install/ccbts_webots/lib/python3.8/site-packages
 
 # Source ros2 environment (update bashrc)
-echo "source /home/root/dai_ws/install/setup.bash" >> /home/$USER/.bashrc
-echo "source /home/root/ur_driver_ws/install/setup.bash" >> /home/$USER/.bashrc
-echo "source /home/root/cocobots_ws/install/setup.bash" >> /home/$USER/.bashrc
+echo "source /ccbts/dai_ws/install/setup.bash" >> /ccbts/.bashrc
+echo "source /ccbts/ur_driver_ws/install/setup.bash" >> /ccbts/.bashrc
+echo "source /ccbts/cocobots_ws/install/setup.bash" >> /ccbts/.bashrc
 
 echo "Dependencies installed"
